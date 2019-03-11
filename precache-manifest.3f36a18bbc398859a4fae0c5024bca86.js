91f9f699ce69fae43c18deede92dc982797c7ef2
papinaser <papinaser@gmail.com>
2019-03-11 13:36:06 +0330

    Updates

---
+++ precache-manifest.3f36a18bbc398859a4fae0c5024bca86.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3ce45484ea8551061fea",
+    "url": "/tictactoe/static/js/runtime~main.3ce45484.js"
+  },
+  {
+    "revision": "8936917336d0a46e4eea",
+    "url": "/tictactoe/static/js/main.89369173.chunk.js"
+  },
+  {
+    "revision": "170d78dc7eaf86966fd3",
+    "url": "/tictactoe/static/js/2.170d78dc.chunk.js"
+  },
+  {
+    "revision": "8936917336d0a46e4eea",
+    "url": "/tictactoe/static/css/main.2175aabd.chunk.css"
+  },
+  {
+    "revision": "aec2c92d89e34888e70763f2c4b130b9",
+    "url": "/tictactoe/index.html"
+  }
+];
